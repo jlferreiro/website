534eed58503065b17ea161d0b9c3fa9698bd296a
jlferreiro <jorgeluisferreiro@BRDG545339MAC.local>
2026-02-12 11:34:25 -0300

    CSS Style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,1 @@
-window.addEventListener("scroll", function() {
-  const navbar = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
-    navbar.style.background = "rgba(11,17,32,0.95)";
-  } else {
-    navbar.style.background = "rgba(11,17,32,0.8)";
-  }
-});
+console.log("JEvolution Loaded");
